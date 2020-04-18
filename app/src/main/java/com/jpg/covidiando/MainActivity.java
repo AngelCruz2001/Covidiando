931b623df77b0692c082e07c9ee2caeb58ff8b14
AngelCruz2001 <34138352+AngelCruz2001@users.noreply.github.com>
2020-04-18 10:36:30 -0500

    implementation

--- app/src/main/java/com/jpg/covidiando/MainActivity.java
+++ app/src/main/java/com/jpg/covidiando/MainActivity.java
@@ -4,14 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this,menu.class);
-        startActivity(intent);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(MainActivity.this, Login.class);
+
+                startActivity(intent);
+            }
+        }, 2000);
+
     }
 }
