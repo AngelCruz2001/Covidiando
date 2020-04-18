7b42ee93c77c2c02f6c1fb39a1b3fd529c68b0a7
Angel Retana <40252881+AERM2018@users.noreply.github.com>
2020-04-18 10:13:05 -0600

    fragment_Configuration 1.0
    I created the configuration fragment

--- app/src/main/java/com/jpg/covidiando/ui/configuracion/ConfiguracionFragment.java
+++ app/src/main/java/com/jpg/covidiando/ui/configuracion/ConfiguracionFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.jpg.covidiando.R;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class ConfiguracionFragment extends Fragment {
@@ -23,6 +29,14 @@ public class ConfiguracionFragment extends Fragment {
         configuracionViewModel =
                 ViewModelProviders.of(this).get(ConfiguracionViewModel.class);
         View root = inflater.inflate(R.layout.fragment_configuracion, container, false);
+        TextView btnUpdateData = (TextView) root.findViewById(R.id.tvUpdateData);
+        btnUpdateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Se ha presionado el boton",Toast.LENGTH_LONG).show();
+
+            }
+        });
         //configuracionViewModel = root.findViewById(R.id.recycler_contact);
         /*configuracionViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
